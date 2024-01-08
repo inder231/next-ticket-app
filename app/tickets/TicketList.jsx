187097e60c99476b56208a1c41b48ff84dc3e4b9
inder231 <inder39811@gmail.com>
2024-01-08 16:01:32 +0530

    feat: tickets listing

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 async function getTickets() {
   const res = await fetch("http://localhost:4000/tickets");
   return res.json();
@@ -9,6 +7,21 @@ export default async function TicketList() {
   // async function to fetch data
   const tickets = await getTickets();
 
-  return <div>{
-    }</div>;
+  return (
+    <div>
+      {tickets.length > 0 ? (
+        tickets?.map((ticket) => (
+          <div key={ticket.id} className="card my-5">
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </div>
+        ))
+      ) : (
+        <p className="text-center">There are no open tickets, yay!!!!</p>
+      )}
+    </div>
+  );
 }
